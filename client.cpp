84b3697e96db1b86b6969dca15c7bf202e13710a
orianezra <orianezra2015@gmail.com>
2017-01-11 09:37:14 -0800

    updated files

--- client.cpp
+++ client.cpp
@@ -5,12 +5,14 @@
 #include "Udp.h"
 #include "Cab.h"
 #include "Driver.h"
+#include "Tcp.h"
 #include "MaterialStatus.h"
 using namespace std;
+
 int main(int argc, char *argv[]) {
     //initiallize udp for message getting
-    Udp udpClient(0, atoi(argv[1]));
-    udpClient.initialize();
+    Tcp tcpClient(0, atoi(argv[1]));
+    tcpClient.initialize();
     //setting variables
     char dummy, status;
     MaterialStatus materialStatus;
@@ -41,36 +43,43 @@ int main(int argc, char *argv[]) {
     Driver* driver = new Driver(id, age, exp, materialStatus, vehicleId);
 
     driver->save();
-    udpClient.sendData(driver->serial_str);
+    sleep(1);
+    tcpClient.sendData(driver->serial_str);
     char buffer[2000];
-    udpClient.reciveData(buffer, sizeof(buffer));
+    tcpClient.reciveData(buffer, sizeof(buffer));
     string stMess(buffer);
-    //cout << stMess <<endl;//thenks for
-    char buffer2[1];
-    udpClient.reciveData(buffer2, sizeof(buffer2));//this is for getting the cars type
+    cout << stMess <<endl;//thenks for
+    char buffer2[10];
+    tcpClient.reciveData(buffer2, sizeof(buffer2));//this is for getting the cars type
     string stCarType(buffer2);
 
-    if(stCarType == "1") {
+    if(stCarType.at(0) = '1') {
         //set the driver's cab
-        udpClient.reciveData(buffer, sizeof(buffer));
+        sleep(1);
+        tcpClient.sendData("getting cab");
+        tcpClient.reciveData(buffer, sizeof(buffer));
         string stMessCab(buffer, sizeof(buffer));
         Cab cabDummy;
+        
         cabDummy.setString(stMessCab);
         Cab* cabO = new Cab();
         cabO->setCab(cabDummy.load());
         driver->setTexi(cabO);
-        udpClient.sendData("we got the cab!");
+        sleep(1);
+        tcpClient.sendData("we got the cab!");
 
     } else {
         //set the driver's cab
-        udpClient.reciveData(buffer, sizeof(buffer));
+        sleep(1);
+        tcpClient.reciveData(buffer, sizeof(buffer));
         string stMessCab(buffer, sizeof(buffer));
         LuxuryCab cabDummy;
         cabDummy.setString(stMessCab);
         LuxuryCab* luxuryCabO = new LuxuryCab();
         luxuryCabO->setLuxuryCab(cabDummy.load());
         driver->setTexi(luxuryCabO);
-        udpClient.sendData("we got the luxurycab!");
+        sleep(1);
+        tcpClient.sendData("we got the luxurycab!");
     }
 
     string stMessage, stMoveBy;
@@ -80,43 +89,45 @@ int main(int argc, char *argv[]) {
         do {
             //wait for a message to get the trip
             char buffer0[2000];
-            udpClient.reciveData(buffer0, sizeof(buffer0));
+            tcpClient.reciveData(buffer0, sizeof(buffer0));
             string stMessage(buffer0);
             if (stMessage.compare("start triping shimi") == 0) {
                 char buffer1[40000];
-                udpClient.reciveData(buffer1, sizeof(buffer1));
+                tcpClient.reciveData(buffer1, sizeof(buffer1));
                 string stMessTrip(buffer1, sizeof(buffer1));
 
                 tDummy.setString(stMessTrip);
                 t->setTripInfo(tDummy.load());
                 driver->setTripInfo(t);
                 break;
-                udpClient.sendData("we got the shimi job!! :)");
+                tcpClient.sendData("we got the shimi job!! :)");
             }
         } while (stMessage.compare("start triping shimi") != 0);
 
         do {
             //wait for a message to drive
             char buffer4[1024];
-            udpClient.reciveData(buffer4, sizeof(buffer4));
+            tcpClient.reciveData(buffer4, sizeof(buffer4));
             string stMoveBy(buffer4);
             if (stMoveBy.compare("you can drive :)") == 0) {
                 driver->drive(driver->getTripInfo()->getWay().front());
-                udpClient.sendData("drive one step");
+                sleep(1);
+                tcpClient.sendData("drive one step");
             }
             if (stMoveBy.compare("end of trip") == 0) {
                 break;
             }
         } while (driver->getTripInfo()->getWay().size() > 0);
-        udpClient.sendData("finnish the drive");
+        sleep(1);
+        tcpClient.sendData("finnish the drive");
         char buffer0[2000];
-        udpClient.reciveData(buffer0, sizeof(buffer0));
+        tcpClient.reciveData(buffer0, sizeof(buffer0));
         string stMessage(buffer0);
         if (stMessage.compare("go home") == 0){
             break;
         }
     }while (stMessage.compare("go home") != 0);
-    udpClient.~Socket();
+    tcpClient.~Socket();
     //delete driver;
 
 }
